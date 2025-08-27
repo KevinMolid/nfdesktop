0030fb9e0778a11190c727c00d6c814c4856ac96
Kevin Molid <kevinmolid@gmail.com>
2025-08-27 17:48:16 +0200

    Improved radio button (not finished)

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -3,25 +3,19 @@ import "./RadioButton.css";
 const RadioButton = ({
   value,
   onChange,
-  labels
+  labels,
 }: {
   value: string;
   onChange: (newValue: string) => void;
   labels: [string, string, string];
 }) => {
   return (
-    <div>
-        <div className="label">
-            {labels[0]}
-        </div>
-        <div className="label">
-            {labels[1]}
-        </div>
-        <div className="label">
-            {labels[2]}
-        </div>
+    <div className="radiobutton">
+      <div className="label label-left">{labels[0]}</div>
+      <div className="label">{labels[1]}</div>
+      <div className="label label-right">{labels[2]}</div>
     </div>
-  )
-}
+  );
+};
 
-export default RadioButton
+export default RadioButton;
